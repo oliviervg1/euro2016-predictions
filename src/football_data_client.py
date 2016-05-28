3e299d0888d0d151f2e4a50b665aec82971577c7
Olivier Van Goethem <olivier.van-goethem@cloudreach.co.uk>
2016-05-29 00:38:19 +0200

    Caching response from football data api

--- src/football_data_client.py
+++ src/football_data_client.py
@@ -8,22 +8,30 @@ class FootballDataApiClient(object):
         self.soccer_season_id = soccer_season_id
         self.requests = requests.Session()
         self.requests.headers.update({"X-Auth-Token": api_key})
+        self._all_teams = None
+        self._all_fixtures = None
 
     def get_all_teams(self):
-        response = self.requests.get(
-            "{0}/soccerseasons/{1}/teams".format(
-                self.base_endpoint, self.soccer_season_id
+        if self._all_teams is None:
+            response = self.requests.get(
+                "{0}/soccerseasons/{1}/teams".format(
+                    self.base_endpoint, self.soccer_season_id
+                )
             )
-        )
-        response.raise_for_status()
-        data = response.json()
-        return [(team["name"], team["crestUrl"]) for team in data["teams"]]
+            response.raise_for_status()
+            data = response.json()
+            self._all_teams = [
+                (team["name"], team["crestUrl"]) for team in data["teams"]
+            ]
+        return self._all_teams
 
     def get_all_fixtures(self):
-        response = self.requests.get(
-            "{0}/soccerseasons/{1}/fixtures".format(
-                self.base_endpoint, self.soccer_season_id
+        if self._all_fixtures is None:
+            response = self.requests.get(
+                "{0}/soccerseasons/{1}/fixtures".format(
+                    self.base_endpoint, self.soccer_season_id
+                )
             )
-        )
-        response.raise_for_status()
-        return response.json()["fixtures"]
+            response.raise_for_status()
+            self._all_fixtures = response.json()["fixtures"]
+        return self._all_fixtures
